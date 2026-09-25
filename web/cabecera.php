820ce912340afaef18e941da58086f1256d51f23
AnasInaniMarouf <ainamar635@g.educaand.es>
2026-09-25 19:36:54 +0200

    Remove offensive language from cabecera.php

--- web/cabecera.php
+++ web/cabecera.php
@@ -1,4 +1,4 @@
-niggerniggernigger<?php 
+<?php 
 define("RUTABASE", dirname(__FILE__));
 //define("MODO_TRABAJO","produccion"); //en "produccion o en desarrollo
 define("MODO_TRABAJO","desarrollo"); //en "produccion o en desarrollo
